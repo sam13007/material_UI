2b932bf0eed28e4df1eebbfd99bb5b5859539947
sam13007 <shyam13007@gmail.com>
2023-03-08 22:58:17 +0530

    changes in master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import ButtonMui from "./components/ButtonMui";
-// import TypographyMui from "./components/TypographyMui";
+import TypographyMui from "./components/TypographyMui";
 
 function App() {
   return (
     <div className="App">
       <ButtonMui />
+      <TypographyMui />
     </div>
   );
 }
