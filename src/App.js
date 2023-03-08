001df3d852575eae4998b3fcb6530c22bd0ad72d
sam13007 <shyam13007@gmail.com>
2023-03-08 21:22:31 +0530

    Typography branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Typography } from "@mui/material";
 
 function App() {
-  return <div className="App">Hello! You are using Material UI</div>;
+  return (
+    <div className="App">
+      <Typography>Hello there we are using Material UI</Typography>
+    </div>
+  );
 }
 
 export default App;
