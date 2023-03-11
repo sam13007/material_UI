744004a65d56d0dfd4ddfd8f4173ba4371993c7a
sam13007 <shyam13007@gmail.com>
2023-03-11 19:29:47 +0530

    Select branch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import TextfieldMui from "./components/TextfieldMui";
 import ButtonMui from "./components/ButtonMui";
 import TypographyMui from "./components/TypographyMui";
-
+import SelectMui from "./components/SelectMui";
 function App() {
   return (
     <div className="App">
       <ButtonMui />
       <TypographyMui />
       <TextfieldMui />
+      <SelectMui />
     </div>
   );
 }
---
+++ src/components/SelectMui.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SelectMui() {
+  return <div>SelectMui</div>;
+}
+
+export default SelectMui;
