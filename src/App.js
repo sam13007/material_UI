1b1d175aa13f83275c0a2490afe82da219099095
sam13007 <shyam13007@gmail.com>
2023-03-11 20:13:52 +0530

    Select changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import TextfieldMui from "./components/TextfieldMui";
-import ButtonMui from "./components/ButtonMui";
-import TypographyMui from "./components/TypographyMui";
 import SelectMui from "./components/SelectMui";
+
 function App() {
   return (
-    <div className="App">
-      <ButtonMui />
-      <TypographyMui />
-      <TextfieldMui />
+    <div>
       <SelectMui />
     </div>
   );
--- src/components/SelectMui.jsx
+++ src/components/SelectMui.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, MenuItem, TextField, Typography } from "@mui/material";
 
 function SelectMui() {
-  return <div>SelectMui</div>;
+  const [selectVal, setSelectVal] = useState([]);
+  const handleSelect = (e) => {
+    const value = e.target.value;
+
+    setSelectVal(typeof value === "string" ? value.split(",") : value);
+  };
+  return (
+    <Box sx={{ textAlign: "center" }} width="450px">
+      <Typography variant="h4">Select using Text field</Typography>
+      <TextField
+        select
+        label="Select your fav IPL team"
+        fullWidth
+        value={selectVal}
+        onChange={handleSelect}
+        SelectProps={{
+          multiple: true,
+        }}
+      >
+        <MenuItem value="MI">MI</MenuItem>
+        <MenuItem value="CSK">CSK</MenuItem>
+        <MenuItem value="KKR">KKR</MenuItem>
+      </TextField>
+    </Box>
+  );
 }
 
 export default SelectMui;
