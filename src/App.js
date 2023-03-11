bf3e9990da124bec50734081c83df9aae82b1396
sam13007 <shyam13007@gmail.com>
2023-03-11 22:47:21 +0530

    Checkbox commited

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
 import CheckboxMui from "./components/CheckboxMui";
 function App() {
   return (
     <div>
-      {/* <TypographyMui />
+      <TypographyMui />
       <ButtonMui />
       <TextfieldMui />
-      <SelectMui /> */}
-      {/* <RadioMui /> */}
+      <SelectMui />
+      <RadioMui />
       <CheckboxMui />
     </div>
   );
