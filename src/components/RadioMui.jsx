fd841f4f74d4fa09323806d51625f93e918b108d
sam13007 <shyam13007@gmail.com>
2023-03-11 22:09:32 +0530

    commit in Radio

--- src/components/RadioMui.jsx
+++ src/components/RadioMui.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+} from "@mui/material";
+import React, { useState } from "react";
 
 function RadioMui() {
-  return <div>RadioMui</div>;
+  const [radioVal, setRadioVal] = useState("");
+  return (
+    <div>
+      <FormControl>
+        <FormLabel>Years of exp</FormLabel>
+        <RadioGroup
+          value={radioVal}
+          onChange={(e) => setRadioVal(e.target.value)}
+        >
+          <FormControlLabel
+            value="0-2"
+            control={<Radio color="success" size="small" />}
+            label="0-2"
+          />
+          <FormControlLabel value="2-4" control={<Radio />} label="2-4" />
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
 }
 
 export default RadioMui;
