510d4515bd34959d1e00033ebc0d4632eb8a85cc
sam13007 <shyam13007@gmail.com>
2023-03-11 20:19:35 +0530

    merged commit'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import ButtonMui from "./components/ButtonMui";
 import SelectMui from "./components/SelectMui";
-
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
 function App() {
   return (
     <div>
+      <TypographyMui />
+      <ButtonMui />
+      <TextfieldMui />
       <SelectMui />
     </div>
   );
