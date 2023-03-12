5e64407d825d9b4660d3c165510387a63c1637e5
sam13007 <shyam13007@gmail.com>
2023-03-12 13:58:14 +0530

    Inputs commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import "./App.css";
 import AutocompleteMui from "./components/AutocompleteMui";
-// import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
-// import RatingMui from "./components/RatingMui";
+import SwitchMui from "./components/SwitchMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
+import RatingMui from "./components/RatingMui";
 
 function App() {
   return (
     <div>
-      {/* <TypographyMui />
+      <TypographyMui />
       <ButtonMui />
       <TextfieldMui />
       <SelectMui />
       <RadioMui />
       <CheckboxMui />
       <SwitchMui />
-      
-      <RatingMui /> */}
+
+      <RatingMui />
       <AutocompleteMui />
     </div>
   );
