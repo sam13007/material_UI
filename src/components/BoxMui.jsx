d1745e64efd0187067b62f96d7c29a768d1fdbb2
sam13007 <shyam13007@gmail.com>
2023-03-12 19:57:08 +0530

    Grid Layout

--- src/components/BoxMui.jsx
+++ src/components/BoxMui.jsx
@@ -1,40 +1,56 @@
-import { Box, Divider, Stack } from "@mui/material";
+import { Box, Divider, Stack, Grid } from "@mui/material";
 import React from "react";
 
 function BoxMui() {
   return (
-    <Stack
-      sx={{ border: "1px  solid", margin: "10px" }}
-      direction="row"
-      divider={<Divider flexItem orientation="vertical" />}
-    >
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "100px",
-        }}
-        height="200px"
-        width="200px"
+    <div style={{ margin: "20px" }}>
+      <Stack
+        sx={{ border: "1px  solid", margin: "10px" }}
+        direction="row"
+        divider={<Divider flexItem orientation="vertical" />}
       >
-        Content
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "yellow",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "100px",
-        }}
-        height="200px"
-        width="200px"
-      >
-        Content
-      </Box>
-    </Stack>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "100px",
+          }}
+          height="200px"
+          width="200px"
+        >
+          Content
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "yellow",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "100px",
+          }}
+          height="200px"
+          width="200px"
+        >
+          Content
+        </Box>
+      </Stack>
+      <Grid container bgcolor="primary.main" textAlign="center" spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Box>Item 1</Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box>Item 2</Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box>Item 3</Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box>Item 4</Box>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
