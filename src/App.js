608456d2fb84d5cde42edde7be6900ba74fd8056
sam13007 <shyam13007@gmail.com>
2023-03-12 19:34:49 +0530

    commiting Stack'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 
 import BoxMui from "./components/BoxMui";
-// import AutocompleteMui from "./components/AutocompleteMui";
-// import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
-// import RatingMui from "./components/RatingMui";
-// import { Typography } from "@mui/material";
+import AutocompleteMui from "./components/AutocompleteMui";
+import SwitchMui from "./components/SwitchMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
+import RatingMui from "./components/RatingMui";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div>
-      {/* <Typography variant="h1" sx={{ textAlign: "center" }}>
+      <Typography variant="h1" sx={{ textAlign: "center" }}>
         Input Components
       </Typography>
       <TypographyMui />
@@ -30,7 +30,7 @@ function App() {
 
       <Typography variant="h1" sx={{ textAlign: "center" }}>
         Layout Components
-  </Typography>*/}
+      </Typography>
       <BoxMui />
     </div>
   );
