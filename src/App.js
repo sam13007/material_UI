3380abff01174fbe847de1bb6e4f8c97fa126041
sam13007 <shyam13007@gmail.com>
2023-03-12 12:18:51 +0530

    Switch merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
 function App() {
   return (
     <div>
-      {/* <TypographyMui />
+      <TypographyMui />
       <ButtonMui />
       <TextfieldMui />
       <SelectMui />
       <RadioMui />
-      <CheckboxMui /> */}
+      <CheckboxMui />
       <SwitchMui />
     </div>
   );
