c36524e2c3d05091ee3b728796d3391590e1eb8e
sam13007 <shyam13007@gmail.com>
2023-03-12 13:04:24 +0530

    Rating system

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-// import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
+import SwitchMui from "./components/SwitchMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
 import RatingMui from "./components/RatingMui";
 function App() {
   return (
     <div>
-      {/* <TypographyMui />
+      <TypographyMui />
       <ButtonMui />
       <TextfieldMui />
       <SelectMui />
       <RadioMui />
       <CheckboxMui />
-      <SwitchMui /> */}
+      <SwitchMui />
       <RatingMui />
     </div>
   );
