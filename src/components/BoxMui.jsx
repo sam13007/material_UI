80346d766dd81c758a7d5718305de4f02f9c1dda
sam13007 <shyam13007@gmail.com>
2023-03-12 19:31:02 +0530

    Stack

--- src/components/BoxMui.jsx
+++ src/components/BoxMui.jsx
@@ -1,21 +1,40 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Stack } from "@mui/material";
 import React from "react";
 
 function BoxMui() {
   return (
-    <Box
-      sx={{
-        backgroundColor: "primary.main",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "100px",
-      }}
-      height="200px"
-      width="200px"
+    <Stack
+      sx={{ border: "1px  solid", margin: "10px" }}
+      direction="row"
+      divider={<Divider flexItem orientation="vertical" />}
     >
-      Content
-    </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "100px",
+        }}
+        height="200px"
+        width="200px"
+      >
+        Content
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "yellow",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "100px",
+        }}
+        height="200px"
+        width="200px"
+      >
+        Content
+      </Box>
+    </Stack>
   );
 }
 
