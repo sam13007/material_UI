eb51d1a9f4d1374231ca62bf0b0b706b6fa1807e
sam13007 <shyam13007@gmail.com>
2023-03-14 21:57:54 +0530

    Imagelist commited

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
-import AccordionMui from "./components/AccordionMui";
+import ImageListMui from "./components/ImageListMui";
+// import AccordionMui from "./components/AccordionMui";
 
-import BoxMui from "./components/BoxMui";
-import AutocompleteMui from "./components/AutocompleteMui";
-import SwitchMui from "./components/SwitchMui";
-import ButtonMui from "./components/ButtonMui";
-import SelectMui from "./components/SelectMui";
-import TypographyMui from "./components/TypographyMui";
-import TextfieldMui from "./components/TextfieldMui";
-import RadioMui from "./components/RadioMui";
-import CheckboxMui from "./components/CheckboxMui";
-import RatingMui from "./components/RatingMui";
-import { Typography } from "@mui/material";
-import CardMui from "./components/CardMui";
+// import BoxMui from "./components/BoxMui";
+// import AutocompleteMui from "./components/AutocompleteMui";
+// import SwitchMui from "./components/SwitchMui";
+// import ButtonMui from "./components/ButtonMui";
+// import SelectMui from "./components/SelectMui";
+// import TypographyMui from "./components/TypographyMui";
+// import TextfieldMui from "./components/TextfieldMui";
+// import RadioMui from "./components/RadioMui";
+// import CheckboxMui from "./components/CheckboxMui";
+// import RatingMui from "./components/RatingMui";
+// import { Typography } from "@mui/material";
+// import CardMui from "./components/CardMui";
+
 function App() {
   return (
     <div>
-      <Typography variant="h1" sx={{ textAlign: "center" }}>
+      {/* <Typography variant="h1" sx={{ textAlign: "center" }}>
         Input Components
       </Typography>
       <TypographyMui />
@@ -34,7 +36,8 @@ function App() {
       </Typography>
       <BoxMui />
       <CardMui />
-      <AccordionMui />
+      <AccordionMui /> */}
+      <ImageListMui />
     </div>
   );
 }
