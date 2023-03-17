2b3cdbf276d4e9348e6e372968990663788246d5
sam13007 <shyam13007@gmail.com>
2023-03-17 20:27:35 +0530

    Menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import "./App.css";
-import ImageListMui from "./components/ImageListMui";
-import AccordionMui from "./components/AccordionMui";
+// import ImageListMui from "./components/ImageListMui";
+// import AccordionMui from "./components/AccordionMui";
 
-import BoxMui from "./components/BoxMui";
-import AutocompleteMui from "./components/AutocompleteMui";
-import SwitchMui from "./components/SwitchMui";
-import ButtonMui from "./components/ButtonMui";
-import SelectMui from "./components/SelectMui";
-import TypographyMui from "./components/TypographyMui";
-import TextfieldMui from "./components/TextfieldMui";
-import RadioMui from "./components/RadioMui";
-import CheckboxMui from "./components/CheckboxMui";
-import RatingMui from "./components/RatingMui";
-import { Typography } from "@mui/material";
-import CardMui from "./components/CardMui";
-import AppbarMui from "./components/AppbarMui";
+// import BoxMui from "./components/BoxMui";
+// import AutocompleteMui from "./components/AutocompleteMui";
+// import SwitchMui from "./components/SwitchMui";
+// import ButtonMui from "./components/ButtonMui";
+// import SelectMui from "./components/SelectMui";
+// import TypographyMui from "./components/TypographyMui";
+// import TextfieldMui from "./components/TextfieldMui";
+// import RadioMui from "./components/RadioMui";
+// import CheckboxMui from "./components/CheckboxMui";
+// import RatingMui from "./components/RatingMui";
+// import { Typography } from "@mui/material";
+// import CardMui from "./components/CardMui";
+// import AppbarMui from "./components/AppbarMui";
+import MenuMui from "./components/MenuMui";
 function App() {
   return (
     <div>
-      <AppbarMui />
+      {/* <AppbarMui />
       <Typography variant="h1" sx={{ textAlign: "center" }}>
         Input Components
       </Typography>
@@ -38,7 +39,8 @@ function App() {
       <BoxMui />
       <CardMui />
       <AccordionMui />
-      <ImageListMui />
+      <ImageListMui /> */}
+      <MenuMui />
     </div>
   );
 }
---
+++ src/components/MenuMui.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Menu, MenuItem, Button } from "@mui/material";
+
+function MenuMui() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="menu-basic"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account </MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default MenuMui;
