b891a6ae805c9d1688fb9e2c9082d08d779468b4
sam13007 <shyam13007@gmail.com>
2023-03-17 20:38:43 +0530

    Link comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import "./App.css";
-// import ImageListMui from "./components/ImageListMui";
-// import AccordionMui from "./components/AccordionMui";
+import ImageListMui from "./components/ImageListMui";
+import AccordionMui from "./components/AccordionMui";
 
-// import BoxMui from "./components/BoxMui";
-// import AutocompleteMui from "./components/AutocompleteMui";
-// import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
-// import RatingMui from "./components/RatingMui";
-// import { Typography } from "@mui/material";
-// import CardMui from "./components/CardMui";
-// import AppbarMui from "./components/AppbarMui";
-// import MenuMui from "./components/MenuMui";
+import BoxMui from "./components/BoxMui";
+import AutocompleteMui from "./components/AutocompleteMui";
+import SwitchMui from "./components/SwitchMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
+import RatingMui from "./components/RatingMui";
+import { Typography } from "@mui/material";
+import CardMui from "./components/CardMui";
+import AppbarMui from "./components/AppbarMui";
+import MenuMui from "./components/MenuMui";
 import LinkMui from "./components/LinkMui";
 function App() {
   return (
     <div>
-      {/* <AppbarMui />
+      <AppbarMui />
       <Typography variant="h1" sx={{ textAlign: "center" }}>
         Input Components
       </Typography>
@@ -41,7 +41,7 @@ function App() {
       <CardMui />
       <AccordionMui />
       <ImageListMui />
-      <MenuMui /> */}
+      <MenuMui />
       <LinkMui />
     </div>
   );
