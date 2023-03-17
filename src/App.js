f2c1b7b8fc05fa78114ea8b74718f266602a1192
sam13007 <shyam13007@gmail.com>
2023-03-17 21:57:26 +0530

    Badge comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
 import AvatarMui from "./components/AvatarMui";
-// import Breadcrumb from "./components/Breadcrumb";
-// import ImageListMui from "./components/ImageListMui";
-// import AccordionMui from "./components/AccordionMui";
-// import BoxMui from "./components/BoxMui";
-// import AutocompleteMui from "./components/AutocompleteMui";
-// import SwitchMui from "./components/SwitchMui";
-// import ButtonMui from "./components/ButtonMui";
-// import SelectMui from "./components/SelectMui";
-// import TypographyMui from "./components/TypographyMui";
-// import TextfieldMui from "./components/TextfieldMui";
-// import RadioMui from "./components/RadioMui";
-// import CheckboxMui from "./components/CheckboxMui";
-// import RatingMui from "./components/RatingMui";
-// import { Typography } from "@mui/material";
-// import CardMui from "./components/CardMui";
-// import AppbarMui from "./components/AppbarMui";
-// import MenuMui from "./components/MenuMui";
-// import LinkMui from "./components/LinkMui";
+import Breadcrumb from "./components/Breadcrumb";
+import ImageListMui from "./components/ImageListMui";
+import AccordionMui from "./components/AccordionMui";
+import BoxMui from "./components/BoxMui";
+import AutocompleteMui from "./components/AutocompleteMui";
+import SwitchMui from "./components/SwitchMui";
+import ButtonMui from "./components/ButtonMui";
+import SelectMui from "./components/SelectMui";
+import TypographyMui from "./components/TypographyMui";
+import TextfieldMui from "./components/TextfieldMui";
+import RadioMui from "./components/RadioMui";
+import CheckboxMui from "./components/CheckboxMui";
+import RatingMui from "./components/RatingMui";
+import { Typography } from "@mui/material";
+import CardMui from "./components/CardMui";
+import AppbarMui from "./components/AppbarMui";
+import MenuMui from "./components/MenuMui";
+import LinkMui from "./components/LinkMui";
 
 function App() {
   return (
     <div>
-      {/* <AppbarMui />
+      <AppbarMui />
       <Typography variant="h1" sx={{ textAlign: "center" }}>
         Input Components
       </Typography>
@@ -45,7 +45,7 @@ function App() {
       <ImageListMui />
       <MenuMui />
       <LinkMui />
-      <Breadcrumb /> */}
+      <Breadcrumb />
       <AvatarMui />
     </div>
   );
